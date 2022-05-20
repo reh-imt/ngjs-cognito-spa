51e106b33d1bf10e54a5bae723452490a62091e4
Roan Horning <rhorning@innovisionmedical.com>
2022-05-20 17:04:06 -0400

    Fix whitespace

--- ngjs-cognito-spa.js
+++ ngjs-cognito-spa.js
@@ -1,17 +1,18 @@
-import Amplify, { Auth, Hub, API } from 'aws-amplify'
-(function(angular) {
+import Amplify, { Auth, Hub, API } from 'aws-amplify';
+
+(function (angular) {
 
     'use strict';
-    var CreateCognitoClientFactory = function  () {
-      return {
-        "Amplify": Amplify,
-        "Auth": Auth,
-        "Hub": Hub,
-        "API": API
+    var CreateCognitoClientFactory = function () {
+        return {
+            "Amplify": Amplify,
+            "Auth": Auth,
+            "Hub": Hub,
+            "API": API
+        }
     }
-}
-  
+
     angular.module('cognito.spa', [])
-        .service('createCognitoClient', function() { return CreateCognitoClientFactory; } )
+        .service('createCognitoClient', function () { return CreateCognitoClientFactory; })
 
 })(window.angular);
